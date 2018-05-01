d8a29505d77643ef3c450d92af7fafd60f2135b3
市川ひなた <hinakami0701@gmail.com>
2018-05-01 16:20:02 +0900

    画面遷移決定

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$(function() {
+    $('#pagepiling').pagepiling({
+        menu: '#nav',
+        sectionSelector: '.section',
+        sectionsColor: ['#fff', '#eee', '#ddd', '#ccc' ,'#aaa'],
+        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+        navigation: {
+            'textColor': '#000',
+            'bulletsColor': '#000',
+            'position': 'right',
+            'tooltips': ['page1', 'page2', 'page3', 'page4', 'page5']
+        },
+    });
+});
