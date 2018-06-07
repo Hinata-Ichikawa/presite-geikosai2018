b91590609df35daacc4c7a5e256798cb060e3182
市川ひなた <hinakami0701@gmail.com>
2018-06-07 15:29:33 +0900

    モーダル反映

--- js/index.js
+++ js/index.js
@@ -2,12 +2,107 @@ $(function() {
     $('#pagepiling').pagepiling({
         menu: '#nav',
         sectionSelector: '.section',
-        anchors: ['page1', 'page2', 'page3', 'page4', 'page5'],
+        anchors: ['top', 'about', 'movie', 'busho', 'welcome_to_geikosai2018'],
         navigation: {
             'textColor': '#000',
             'bulletsColor': '#000',
             'position': 'right',
-            'tooltips': ['page1', 'page2', 'page3', 'page4', 'page5']
         },
     });
 });
+
+$(function(){
+
+//グローバル変数
+var nowModalSyncer = null ;		//現在開かれているモーダルコンテンツ
+var modalClassSyncer = "modal-syncer" ;		//モーダルを開くリンクに付けるクラス名
+
+//モーダルのリンクを取得する
+var modals = document.getElementsByClassName( modalClassSyncer ) ;
+
+//モーダルウィンドウを出現させるクリックイベント
+for(var i=0,l=modals.length; l>i; i++){
+
+	//全てのリンクにタッチイベントを設定する
+	modals[i].onclick = function(){
+
+		//ボタンからフォーカスを外す
+		this.blur() ;
+
+		//ターゲットとなるコンテンツを確認
+		var target = this.getAttribute( "data-target" ) ;
+
+		//ターゲットが存在しなければ終了
+		if( typeof( target )=="undefined" || !target || target==null ){
+			return false ;
+		}
+
+		//コンテンツとなる要素を取得
+		nowModalSyncer = document.getElementById( target ) ;
+
+		//ターゲットが存在しなければ終了
+		if( nowModalSyncer == null ){
+			return false ;
+		}
+
+		//キーボード操作などにより、オーバーレイが多重起動するのを防止する
+		if( $( "#modal-overlay" )[0] ) return false ;		//新しくモーダルウィンドウを起動しない
+		//if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する
+
+		//オーバーレイを出現させる
+		// $( "body" ).append( '<div id="modal-overlay"></div>' ) ;
+		$( "#section4" ).append( '<div id="modal-overlay"></div>' ) ;
+		$( "#modal-overlay" ).fadeIn( "fast" ) ;
+
+		//コンテンツをセンタリングする
+		centeringModalSyncer() ;
+
+		//コンテンツをフェードインする
+		$( nowModalSyncer ).fadeIn( "slow" ) ;
+
+		//[#modal-overlay]、または[#modal-close]をクリックしたら…
+		$( "#modal-overlay,#modal-close" ).unbind().click( function() {
+
+			//[#modal-content]と[#modal-overlay]をフェードアウトした後に…
+			$( "#" + target + ",#modal-overlay" ).fadeOut( "fast" , function() {
+
+				//[#modal-overlay]を削除する
+				$( '#modal-overlay' ).remove() ;
+
+			} ) ;
+
+			//現在のコンテンツ情報を削除
+			nowModalSyncer = null ;
+
+		} ) ;
+
+	}
+
+}
+
+	//リサイズされたら、センタリングをする関数[centeringModalSyncer()]を実行する
+	$( window ).resize( centeringModalSyncer ) ;
+
+	//センタリングを実行する関数
+	function centeringModalSyncer() {
+
+		//モーダルウィンドウが開いてなければ終了
+		if( nowModalSyncer == null ) return false ;
+
+		//画面(ウィンドウ)の幅、高さを取得
+		var w = $( window ).width() ;
+		var h = $( window ).height() ;
+
+		//コンテンツ(#modal-content)の幅、高さを取得
+		// jQueryのバージョンによっては、引数[{margin:true}]を指定した時、不具合を起こします。
+//		var cw = $( nowModalSyncer ).outerWidth( {margin:true} ) ;
+//		var ch = $( nowModalSyncer ).outerHeight( {margin:true} ) ;
+		var cw = $( nowModalSyncer ).outerWidth() ;
+		var ch = $( nowModalSyncer ).outerHeight() ;
+
+		//センタリングを実行する
+		// $( nowModalSyncer ).css( {"left": ((w - cw)/2) + "px","top": ((h - ch)/2) + "px"} ) ;
+
+	}
+
+} ) ;
